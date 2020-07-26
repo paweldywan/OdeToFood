7f40d4fff1868c7e7adf5beb4b901f708dda6401
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-07-26 15:35:14 +0200

    Development

--- OdeToFood.Web/App_Start/RouteConfig.cs
+++ OdeToFood.Web/App_Start/RouteConfig.cs
@@ -11,6 +11,8 @@ namespace OdeToFood.Web
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            //{*pathInfo} oznacza cokolwiek, wszystko. Czyli przykładowo dla "trace.axd/1/2/3/4" parametr "resource" to trace"
+            //natomiast parametr "pathInfo" to 1/2/3/4. Przekierowanie pasuje do szablonu, więc zostanie zignorowane, nie zostanie obsłużone.
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
